512661a537455d8fbc1a8276aea645c289e50a2a
minamisawafumio <fmsoft@yahoo.co.jp>
2021-05-18 10:59:17 +0900

    a

---
+++ docs/wasm-dev-book-hello-wasm/target/wasm32-unknown-unknown/release/deps/wasm_dev_book_hello_wasm.d
@@ -0,0 +1,5 @@
+C:\Users\fmsof\projects\wasm-dev-book-hello-wasm\target\wasm32-unknown-unknown\release\deps\wasm_dev_book_hello_wasm.wasm: src\lib.rs
+
+C:\Users\fmsof\projects\wasm-dev-book-hello-wasm\target\wasm32-unknown-unknown\release\deps\wasm_dev_book_hello_wasm.d: src\lib.rs
+
+src\lib.rs:
---
+++ docs/wasm-dev-book-hello-wasm/target/wasm32-unknown-unknown/release/wasm_dev_book_hello_wasm.d
@@ -0,0 +1,1 @@
+C:\Users\fmsof\projects\wasm-dev-book-hello-wasm\target\wasm32-unknown-unknown\release\wasm_dev_book_hello_wasm.wasm: C:\Users\fmsof\projects\wasm-dev-book-hello-wasm\src\lib.rs
